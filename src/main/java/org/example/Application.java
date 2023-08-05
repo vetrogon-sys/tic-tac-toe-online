75c054b9b0b921fbfeb0a822802a366110a4cb6a
Kostya <60735936+vetrogon-sys@users.noreply.github.com>
2023-08-05 23:29:59 +0300

    Revert "Start menu"

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,29 +1,8 @@
 package org.example;
 
-import org.example.tic_tac_toe.game.Game;
-import org.example.tic_tac_toe.menu.MenuItem;
-import org.example.tic_tac_toe.menu.StartMenu;
-
-import javax.swing.*;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        StartMenu startMenu = new StartMenu();
-        Game game = new Game();
-        startMenu.showMenu(List.of(
-            new MenuItem(1, "Create game", game::start),
-            new MenuItem(2, "Join Game", game::start),
-            new MenuItem(3, "Exit", Application::exit)
-        ));
+    public static void main(String[] args) {
+
     }
-
-    private static void exit(Void unused) {
-        System.exit(1);
-    }
-
-
 }
--- src/main/java/org/example/tic_tac_toe/game/Game.java
+++ src/main/java/org/example/tic_tac_toe/game/Game.java
@@ -1,14 +1,9 @@
 package org.example.tic_tac_toe.game;
 
-import java.util.Scanner;
-
 public class Game {
 
-    private final Scanner SCANNER = new Scanner(System.in);
+    public void start() {
 
-    public void start(Void i) {
-        System.out.print("Enter game name: ");
-        String gameName = SCANNER.next();
     }
 
 }
